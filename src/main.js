2a62f69c3d626c9a073f492e598d74d5a4982bfd
MrBelugaBogdan <medvedovbogdan19@gmail.com>
2026-03-07 22:34:42 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-// Запуск BlockStream
-const engine = new window.GameEngine();
-engine.init();
-
-console.log("BlockStream Engine запущено!");
+// Перевірка наявності ядра перед запуском
+if (window.GameEngine) {
+    const engine = new window.GameEngine();
+    engine.init();
+    console.log("BlockStream Engine запущено успішно!");
+} else {
+    console.error("Помилка: GameEngine не знайдено. Перевірте engine.js");
+}
