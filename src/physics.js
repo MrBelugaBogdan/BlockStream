bfd307f707d6da8acb0d73bf10b676638b107d8a
MrBelugaBogdan <medvedovbogdan19@gmail.com>
2026-03-11 18:08:53 +0300

    Update physics.js

--- src/physics.js
+++ src/physics.js
@@ -8,8 +8,8 @@ export class Physics {
         this.raycaster = new THREE.Raycaster();
     }
 
-    update(camera, scene, keys, controls, delta) {
-        // Жорстка гравітація
+    update(camera, scene, keys, controls, delta, engine) {
+        const prevVelocityY = this.velocity.y;
         this.velocity.y -= 28.0 * delta; 
 
         if (keys['KeyW']) controls.moveForward(0.12);
@@ -19,14 +19,17 @@ export class Physics {
 
         camera.position.y += this.velocity.y * delta;
 
-        // Промінь вниз для перевірки підлоги
         this.raycaster.set(camera.position, new THREE.Vector3(0, -1, 0));
         const hits = this.raycaster.intersectObjects(scene.children);
         
         if (hits.length > 0) {
             const hitDist = hits[0].distance;
-            // Якщо ноги нижче рівня землі — виштовхуємо вгору
             if (hitDist < this.playerHeight) {
+                // ПЕРЕВІРКА УРОНУ ВІД ПАДІННЯ
+                if (prevVelocityY < -15 && engine.gameMode === 'survival') {
+                    engine.takeDamage(Math.floor(Math.abs(prevVelocityY) / 5));
+                }
+                
                 this.velocity.y = 0;
                 camera.position.y += (this.playerHeight - hitDist);
                 this.canJump = true;
@@ -35,15 +38,8 @@ export class Physics {
             this.canJump = false;
         }
 
-        // Захист від падіння в безодню
-        if (camera.position.y < -20) {
-            camera.position.set(4, 30, 4);
-            this.velocity.y = 0;
-        }
-
-        // Стрибок (тільки якщо торкаємося землі)
         if (keys['Space'] && this.canJump) {
-            this.velocity.y = 11;
+            this.velocity.y = 10.5;
             this.canJump = false;
         }
     }
