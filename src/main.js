82f6a84e858b7f78acc1ec5704018a896e8c2d91
MrBelugaBogdan <medvedovbogdan19@gmail.com>
2026-03-15 22:05:03 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,4 @@
-import { GameEngine } from './engine.js';
+import { GameEngine } from './src/engine.js';
 
 const game = new GameEngine();
-game.init().catch(err => console.error("Помилка запуску:", err));
-
-// Клік для входу в гру
-document.addEventListener('click', () => {
-    game.controls.lock();
-});
+game.init();
